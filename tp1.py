7d1d70fe3735dedc2102e718f8c918a848e08978
tomasroko1 <tomas.e.roko@gmail.com>
2025-02-12 17:02:34 -0300

    primera métrica

--- tp1.py
+++ tp1.py
@@ -20,3 +20,36 @@ padron_poblacion = pd.read_excel('padron_poblacion.xlsx')
 
 #%%
 
+""" ANOTACIONES (ELIMINAR AL FINAL)
+
+1) La base de datos 'padron_población' ES MALÍSIMA. No solo porque las cosas no están organizadas como corresponde,
+si no porque usan un código de area identificador que por lo que estuve viendo NO ES el mismo que el codigo que tienen
+'censos_culturales' y 'padron_poblacion' llamado 'Cod_loc' que parece ser el código de 'localidad censal'
+
+2) El objetivo del informe es analizar la relación que hay entre #centros educativos y #centros culturales
+    en cada PROVINCIA. Por lo tanto, el dato de 'departamento' no nos va a importar, tampoco el de 'localidad'
+
+"""
+
+# CHEQUEO QUE ESTO SEA UNA CLAVE
+consultaSQL = dd.sql(
+    """
+    SELECT DISTINCT Nombre, Latitud, Longitud
+    FROM centros_culturales
+    """
+    ).df()
+
+
+# Elimino el espacio vacío en 'Mail '
+centros_culturales.columns = centros_culturales.columns.str.strip()
+
+# METRICA PARA LA CALIDAD DEL DATO 'Mail' EN LA TABLA 'centros_culturales'
+metrica01 = dd.sql(
+    """
+    SELECT COUNT(*) * 100 / (SELECT COUNT(*) FROM centros_culturales) AS porcentaje_nulls
+    FROM centros_culturales
+    WHERE Mail IS NULL
+    OR (Mail IN ('-', 's/d'))
+    """
+    ).df()
+
